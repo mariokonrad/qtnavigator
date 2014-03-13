36ec58ed225901f6687c92a0f8906b7716f2fdbe
Mario Konrad <mario.konrad@gmx.net>
2014-03-13 21:20:07 +0100

    Printing application path on startup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <MainWindow.hpp>
 #include <plugins/DefaultManager.hpp>
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
+
+	qDebug() << "ApplicationDirPath: " << app.applicationDirPath();
 
 	plugins::DefaultManager plugins;
 	plugins.load(app.applicationDirPath() + "/../lib/");
